7ea60c49d9a731ecb21a2f5dd09d339408a7d52c
Josh <57708928+designedbyjosh@users.noreply.github.com>
2020-11-29 23:04:28 +1100

    🎉 Added support for article thumbnails on the home page

---
+++ src/Content/Pages/About/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import Box from '../../../Components/Box';
+import Grid from '../../../Components/Grid'
+
+function About() {
+  return (
+    <div className="App">
+      Stuff goes here
+    </div>
+  );
+}
+
+export default About;
