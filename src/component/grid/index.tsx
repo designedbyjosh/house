ead6117606d854960bfe2fdd2815f1a518ff2281
Josh <57708928+designedbyjosh@users.noreply.github.com>
2020-12-06 20:46:16 +1100

    🧹 No more error messages

--- src/component/grid/index.tsx
+++ src/component/grid/index.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { AnimatePresence, motion } from 'framer-motion';
+import React from 'react';
+import {  motion } from 'framer-motion';
 import './grid.scss';
 import { fade, staggered } from '../../styling/animations/transitions';
 
