8822243397d40a7a8e7bab9eb9fa3a716ffb8113
Josh <57708928+designedbyjosh@users.noreply.github.com>
2021-01-31 14:51:48 +1100

    🛠️ Added custom analytics monitoring

--- pages/_document.js
+++ pages/_document.js
@@ -10,6 +10,7 @@ class HouseDocument extends Document {
     return (
       <Html>
         <Head>
+          <script async defer data-website-id="75cceb23-141b-4fa3-8ea9-cbc2b0a65eb2" src="https://josh-umami.herokuapp.com/umami.js"></script>
           <link rel="preconnect" href="https://fonts.gstatic.com" /> 
           <link href="https://fonts.googleapis.com/css2?family=Inter:display=swap" rel="stylesheet" />
           <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet"></link> 
