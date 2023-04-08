41400ad47a4c3b8fe0b517859895425880e7a88f
Josh <57708928+designedbyjosh@users.noreply.github.com>
2023-04-08 15:55:38 +1000

    🎸 Add Support for Spotify Status (#12)
    * Update for 2023
    
    * Fix failed builds
    
    * Fix failed builds
    
    * Bump to new version of next and new style
    
    * Fix unescaped entity
    
    * Fix unescaped entity
    
    * Add support for a photo view
    
    * Add support for a photo view
    
    * Add Spotify streaming
    
    * Add Spotify streaming
    
    * Resolve conflicts
    
    * Fix padding
    
    * Fix track name
    
    * Fix track name

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,18 +1,26 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChartLine, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import Container from './container';
+import Link from 'next/link';
+import NowPlaying from './now-playing';
 
-export default function Header({}) {
+export default function Header({currentPage} : {currentPage: string}) {
+
   return (
-    <>
+    <Container>
         <div className="flex pt-8 pb-4">
             <div className="flex-1 left-0">
                 <h1 className="text-4xl md:text-6xl  md:pt-16">
+                    <Link href="/">
                     Joshua Whitcombe
+                    </Link>
                 </h1>
             </div>
             <div className="flex justify-end flex-col">
                 <div className="h-9 md:h-10">
-                <FontAwesomeIcon className='pr-2' size='lg' icon={faEnvelope} />
+                <a className="hover:opacity-50" href="mailto:hello@josh.house">
+                    <FontAwesomeIcon className='pr-2' size='lg' icon={faEnvelope} />
+                </a>
                 <a className="hover:opacity-50" href="https://umami.josh.house/share/Vru7iEaq/Josh's%20Blog">
                     <FontAwesomeIcon className='pr-2 pl-2' size='lg' icon={faChartLine} />
                 </a>
@@ -20,17 +28,23 @@ export default function Header({}) {
             </div>
         </div>
         
-        <h3 className="text-xl pb-6">
-        <button className="bg-stone-700 hover:bg-stone-800 text-white py-1 px-4 rounded">
-            Photography
-        </button>
-        <button disabled className="bg-stone-800 text-white/20 py-1 px-4 ml-2 rounded">
-            Blog
-        </button>
-        <button disabled className="bg-stone-800 text-white/20 py-1 px-4 ml-2 rounded">
-            Analytics
-        </button>
-        </h3>
-    </>
+        <div className="flex pb-4">
+            <div className="flex-1 inline left-0">
+                <Link href="/">
+            <button className={`${currentPage == "/" || currentPage == "" && "bg-stone-500 !text-white"} hover:bg-stone-800 hover:text-white text-black dark:text-white py-1 px-4 rounded`}>
+                Photography
+            </button>
+            </Link>
+            <Link href="/blog">
+            <button className={`${currentPage == "/blog" && "bg-stone-500 !text-white"} ml-2  hover:bg-stone-800 hover:text-white text-black dark:text-white py-1 px-4 rounded`}>
+                Blog
+            </button>
+            </Link>
+            </div>
+            <div className="flex justify-end flex-col">
+            <NowPlaying />
+            </div>
+        </div>
+    </Container>
   )
 }
---
+++ src/components/now-playing.tsx
@@ -0,0 +1,47 @@
+import { socket } from "@/lib/spotapi"
+import { faSpotify } from "@fortawesome/free-brands-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { useEffect, useState } from "react"
+import { motion } from 'framer-motion'
+import { Tooltip } from '@nextui-org/react';
+import moment from "moment"
+import Link from "next/link"
+
+export default function NowPlaying() {
+
+    const [music, setMusic] = useState({} as any)
+
+    socket.on('refresh', (data) => {
+        setMusic(data)
+    })
+
+    const currentSong = music.item?.name
+
+    return music.now_playing ? <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        layout
+    >
+        <Link href="https://github.com/designedbyjosh/spotapi">
+            <Tooltip content={"This is being updated live using my Spotapi service"} rounded>
+                <FontAwesomeIcon icon={faSpotify} size="lg" className="mr-2 text-green-600" beatFade />
+                <motion.span layout >{currentSong}</motion.span>
+            </Tooltip>
+        </Link>
+    </motion.div> :
+        <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            layout
+        >
+            <Link href="https://github.com/designedbyjosh/spotapi">
+                <Tooltip content={`I stopped listening ${moment(music.timestamp).fromNow()}`} rounded>
+                    <FontAwesomeIcon icon={faSpotify} size="lg" className="mr-2 text-stone-600"/>
+                    <motion.span layout className="text-stone-600">{currentSong}</motion.span>
+                </Tooltip>
+            </Link>
+        </motion.div>
+
+}
