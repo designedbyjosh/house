ee0a649c655ce95464253f759b12ea6bc66535a9
Joshua Whitcombe <57708928+designedbyjosh@users.noreply.github.com>
2023-04-08 15:27:30 +1000

    Fix padding

--- src/components/container.tsx
+++ src/components/container.tsx
@@ -2,6 +2,6 @@ import { ReactNode } from "react";
 
 export default function Container({ children } : {children: ReactNode}) {
     return <div className="container justify-center flex min-w-full">
-        <div className="container max-w-screen-xl">{children}</div>
+        <div className="container max-w-screen-xl px-10 med:px-0">{children}</div>
       </div>
   }
