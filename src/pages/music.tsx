69d6d27e2fb591d7505962314a8434cc7b6c1d6e
Joshua Whitcombe <57708928+designedbyjosh@users.noreply.github.com>
2023-04-10 08:58:05 +1000

    Hotfix rate limit errors

--- src/pages/music.tsx
+++ src/pages/music.tsx
@@ -23,30 +23,28 @@ export default function Music({ music }: music) {
       </Head>
       <Container>
         <motion.div initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        exit={{ opacity: 0 }} className="grid grid-cols-2 gap-10 md:grid-cols-5">
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }} className="grid grid-cols-2 gap-10 md:grid-cols-5">
           {music.top_tracks?.items.map((track, id) => (
             <motion.div initial={{ opacity: 0 }}
-            animate={{ opacity: 1, transition: { delay: id*0.05} }}
-            exit={{ opacity: 0 }} key={track.name}>
-            <Zoom><img  alt={track.name} className="pt-2 md:pt-4" src={track.album.images[0].url} /></Zoom>
-            <Link href={track.external_urls.spotify}><p className="tracking-wider text-small py-4  text-gray-400/75">{id+1}. {track.name}</p></Link>
-          </motion.div>
+              animate={{ opacity: 1, transition: { delay: id * 0.05 } }}
+              exit={{ opacity: 0 }} key={track.name}>
+              <Zoom><img alt={track.name} className="pt-2 md:pt-4" src={track.album.images[0].url} /></Zoom>
+              <Link href={track.external_urls.spotify}><p className="tracking-wider text-small py-4  text-gray-400/75">{id + 1}. {track.name}</p></Link>
+            </motion.div>
           ))}
-          
-          </motion.div>
+
+        </motion.div>
       </Container>
     </>
   )
 }
 
-export const getStaticProps: GetStaticProps = async () => {
-    return new Promise(async function(resolve) {
-        await socket.emit('immediate_refresh_request', (music: SpotapiObject) => {
-            resolve({
-                props: { music },
-                revalidate: 30,
-            });
-          })
-    })
-}
+export const getStaticProps: GetStaticProps = async () => new Promise(async function (resolve) {
+  await socket.emit('immediate_refresh_request', (music: SpotapiObject) => {
+    resolve({
+      props: { music },
+      revalidate: 30,
+    });
+  })
+})
