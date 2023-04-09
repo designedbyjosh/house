90218063fac97e694183018c0203a9fbd880dcca
Joshua Whitcombe <57708928+designedbyjosh@users.noreply.github.com>
2023-04-09 16:38:42 +1000

    Improve music support

--- src/components/now-playing.tsx
+++ src/components/now-playing.tsx
@@ -1,4 +1,4 @@
-import { socket } from "@/lib/spotapi"
+import { SpotapiObject, socket } from "@/lib/spotapi"
 import { faSpotify } from "@fortawesome/free-brands-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { useEffect, useState } from "react"
@@ -9,41 +9,33 @@ import Link from "next/link"
 
 export default function NowPlaying() {
 
-    const [music, setMusic] = useState({} as any)
+    const [music, setMusic] = useState({now_playing: {}, top_tracks: {}, top_artists: {}} as SpotapiObject)
 
-    useEffect(() => {socket.emit('immediate_refresh_request')}, [])
+    useEffect(() => {socket.emit('immediate_refresh_request', (data: any) => setMusic(data))}, []);
 
+    // Handles any incoming request for music updates
     socket.on('update', (data) => {
         setMusic(data)
     })
 
-    const currentSong = music?.item?.name
-
-    return music?.now_playing ? <motion.div
+    const main = (tooltip: string, color: string, currentSong: string, playing: boolean) => <motion.div
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         exit={{ opacity: 0 }}
         layout
     >
         <Link href="https://github.com/designedbyjosh/spotapi">
-            <Tooltip content={"This is being updated live using my Spotapi service"} rounded>
-                <FontAwesomeIcon icon={faSpotify} size="lg" className="mr-2 text-green-600" beatFade />
-                <motion.span layout >{currentSong}</motion.span>
+            <Tooltip enterDelay={200} content={tooltip} rounded>
+                <FontAwesomeIcon icon={faSpotify} size="lg" className={`mr-2 ${color}`} beatFade={playing} />
+                <motion.span className={`${playing ? 'text-black dark:text-white' : color}`} layout >{currentSong}</motion.span>
             </Tooltip>
         </Link>
-    </motion.div> :
-        <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-            layout
-        >
-            <Link href="https://github.com/designedbyjosh/spotapi">
-                <Tooltip content={`I stopped listening ${moment(music.timestamp).fromNow()}`} rounded>
-                    <FontAwesomeIcon icon={faSpotify} size="lg" className="mr-2 text-stone-600"/>
-                    <motion.span layout className="text-stone-600">{currentSong}</motion.span>
-                </Tooltip>
-            </Link>
-        </motion.div>
+    </motion.div>
+
+    if (Object.keys(music?.now_playing!).length === 0) return <div/>
+
+    return music?.now_playing?.is_playing
+        ? main("This is being updated live using my Spotapi service", "text-green-600", music.now_playing?.item?.name!, true)
+        : main(`I stopped listening ${moment(music?.now_playing?.timestamp).fromNow()}`, "text-stone-600", music.now_playing?.item?.name!, false)
 
 }
--- src/lib/spotapi.ts
+++ src/lib/spotapi.ts
@@ -1,3 +1,9 @@
 import { io } from 'socket.io-client';
 
+export interface SpotapiObject {
+    now_playing?: SpotifyApi.CurrentlyPlayingObject,
+    top_tracks?: SpotifyApi.UsersTopTracksResponse,
+    top_artists?: SpotifyApi.UsersTopArtistsResponse
+}
+
 export const socket = io("https://spotify.josh.house", { path: "/ws"});
