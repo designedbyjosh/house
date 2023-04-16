a8049b573d9018c2a4abb7c227f3d068ac4d5130
Joshua Whitcombe <57708928+designedbyjosh@users.noreply.github.com>
2023-04-16 10:22:47 +1000

    Add support for post featuring

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,28 +17,30 @@ export default function Index({ photos }: index) {
 
   let [hoveredImage, setHoveredImage] = useState('');
 
+  const featured = photos.find(photo => photo.featured);
+
   return (
     <>
       <Head>
         <title>{`Josh Lives Here`}</title>
       </Head>
       <Container>
-        <motion.div 
+        {featured && <motion.div 
           initial={{ opacity: 0 }}
-          animate={{opacity: ((hoveredImage === '' || hoveredImage === photos[0].id) ? 1 : 0.5)}}
+          animate={{opacity: ((hoveredImage === '' || hoveredImage === featured!.id) ? 1 : 0.5)}}
           exit={{ opacity: 0 }} 
           className="max-w-screen-xl flex flex-col">
             <Zoom classDialog='custom-zoom'>
-              <img onMouseEnter={() => setHoveredImage(photos[0].id)} onMouseLeave={() => {setHoveredImage('')}} alt={photos[0].feature_image_alt!} src={photos[0].feature_image!} />
-              <p className="tracking-wider text-small py-4 text-gray-400/75">{moment(photos[0].created_at).format("MMMM Do, YYYY") + " // " + photos[0].feature_image_caption!}</p>
+              <img onMouseEnter={() => setHoveredImage(featured!.id)} onMouseLeave={() => {setHoveredImage('')}} alt={featured!.feature_image_alt!} src={featured!.feature_image!} />
+              <p className="tracking-wider text-small py-4 text-gray-400/75">{moment(featured!.created_at).format("MMMM Do, YYYY") + " // " + featured!.feature_image_caption!}</p>
             </Zoom>
-        </motion.div>
+        </motion.div>}
         <motion.div initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         exit={{ opacity: 0 }} className="grid grid-cols-1 gap-x-10 md:grid-cols-2">
-          {photos.slice(1).map((photo) => (
+          {photos.filter(photo => !photo.featured).map((photo) => (
             <motion.div animate={{opacity: ((hoveredImage === '' || hoveredImage === photo.id) ? 1 : 0.5)}} style={{borderRadius: '5px'}} key={photo.uuid}>
-            <Zoom classDialog='custom-zoom'><img onMouseEnter={() => setHoveredImage(photo.id)} onMouseLeave={() => {setHoveredImage('')}} style={{height: '300px', minWidth: '100%', objectFit: "cover"}} alt={photo.feature_image_alt!} className="pt-2 md:pt-4" src={photo.feature_image!} /></Zoom>
+            <Zoom classDialog='custom-zoom'><img onMouseEnter={() => setHoveredImage(photo.id)} onMouseLeave={() => {setHoveredImage('')}} style={{ objectFit: "cover"}} alt={photo.feature_image_alt!} className="pt-2 md:pt-4" src={photo.feature_image!} /></Zoom>
             <p className="tracking-wider text-small py-5 mb-1 text-gray-400/75">{moment(photo.published_at).format("MMMM Do, YYYY") + " // " + photo.feature_image_caption!}</p>
           </motion.div>
           ))}
